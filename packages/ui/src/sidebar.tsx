d670a774789806750a1d94b5680a89f97c98a259
Sayazrahman <115418700+Sayazrahman@users.noreply.github.com>
2025-09-17 14:20:36 +0530

    (fix): div error

--- packages/ui/src/sidebar.tsx
+++ packages/ui/src/sidebar.tsx
@@ -207,7 +207,7 @@ function Sidebar({
 
   return (
     <div
-      className="group peer text-sidebar-foreground hidden"
+      className="group peer text-sidebar-foreground"
       data-state={state}
       data-collapsible={state === "collapsed" ? collapsible : ""}
       data-variant={variant}
